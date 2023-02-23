1e4952b2b120490f6a329d828ceb8099e2d7ea94
hyunzsu <kkm5169@naver.com>
2023-02-24 00:21:54 +0900

    [Add] DiaryList 컴포넌트 제작 - map 사용해서 list 형식으로 렌더링 - 렌더링된 list 컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: '현지수',
+    content: '하이 1',
+    emotion: 5,
+    created_date: new Date().getTime(), // 현재 시간 기준으로 생성
+  },
+  {
+    id: 2,
+    author: '박윤하',
+    content: '하이 2',
+    emotion: 3,
+    created_date: new Date().getTime(), // 현재 시간 기준으로 생성
+  },
+  {
+    id: 3,
+    author: '김연주',
+    content: '하이 3',
+    emotion: 1,
+    created_date: new Date().getTime(), // 현재 시간 기준으로 생성
+  }
+];
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <DiaryEditor />
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
---
+++ src/DiaryList.js
@@ -0,0 +1,23 @@
+import DiaryItem from './DiaryItem';
+
+const DiaryList = ({ diaryList }) => {
+  console.log(diaryList);
+  return (
+    <div className='DiaryList'>
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => ( // it객체에 데이터가 DiaryItem의 props로 전달
+          <DiaryItem key={it.id} {...it}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// defaultProps -> undefined로 전달될 것 같은 props를 기본값으로 설정
+DiaryList.defaultProps = {
+  diaryList: []
+}
+
+export default DiaryList;
