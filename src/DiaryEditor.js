e7d4401142f27501f693bafc6379ecde06c07aea
hyunzsu <kkm5169@naver.com>
2023-02-23 16:29:10 +0900

    [Add] useRef - React에서 DOM 요소 접근하기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,10 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const DiaryEditor = () => {
+
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: '',
     content: '',
@@ -18,7 +22,18 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      // focus
+      authorInput.current.focus(); 
+      return;
+    }
+    
+    if (state.content.length < 5) {
+      // focus
+      contentInput.current.focus();
+      return;
+    }
+
     alert('저장 성공!')
   }
 
@@ -27,6 +42,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name='author'
           value={state.author}
           onChange={handleChangeState}
@@ -34,6 +50,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name='content'
           value={state.content}
           onChange={handleChangeState}
