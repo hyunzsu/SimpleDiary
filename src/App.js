a3dc31080834a7cc2ec5206a1030106b8e4ac6cf
hyunzsu <kkm5169@naver.com>
2023-05-19 20:05:17 +0900

    [Add] useReducer 이용 - 복잡한 상태 관리 로직 분리하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import { useState, useRef, useEffect, useMemo, useCallback } from 'react';
+import { useRef, useEffect, useMemo, useCallback, useReducer } from 'react';
 // import Lifecycle from './Lifecycle';
 // import OptimizeTest from './OptimizeTest';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INIT': {
+      return action.data;
+    }
+    case 'CREATE': {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_date,
+      };
+      return [newItem, ...state];
+    }
+    case 'REMOVE': {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case 'EDIT': {
+      return state.map((it) =>
+        it.id === action.targetId ? { ...it, content: action.newContent } : it
+      );
+    }
+    // 'EDIT' type의 action이 발생하면 action으로 targetId와 newContent가 전달, map을 통해 수정하는 부분content만 newContent로 수정하고 나머지 요소는 그대로 돌려줌
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const [data, setData] = useState([]); // 일기 없는 상태로 시작할거니까 빈배열, 일기상태변화함수 - setData
+  // const [data, setData] = useState([]); // 일기 없는 상태로 시작할거니까 빈배열, 일기상태변화함수 - setData
+
+  const [data, dispatch] = useReducer(reducer, []);
 
   // id 추가하는거 useRef 이용
   const dataId = useRef(0); // 초기값 0 설정
@@ -30,7 +59,8 @@ function App() {
       };
     });
 
-    setData(initData); // 일기데이터 초기값 설정
+    dispatch({ type: 'INIT', data: initData }); // reducer함수는 action객체를 갖는데 action의 type:'INIT'이고, 그 action의 필요한 data: initData가 됨
+    // setData(initData); // 일기데이터 초기값 설정
   };
 
   // (1) 컴포넌트 mount 시점에 API를 호출하는 getData를 전달해줌
@@ -39,34 +69,31 @@ function App() {
   }, []);
 
   const onCreate = useCallback((author, content, emotion) => {
-    const created_date = new Date().getTime(); // 현재시간 구해버려
+    dispatch({
+      type: 'CREATE',
+      data: { author, content, emotion, id: dataId.current },
+    });
 
-    // 새로운 일기아이템으로 추가해야 하는 것
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      id: dataId.current,
-    };
     dataId.current += 1; // 다음 일기 id는 1을 가져야 하니까 증가시킴
-    setData((data) => [newItem, ...data]); // 원래 배열에 있던 데이터들을 하나하나 나열, 새로 추가할 일기 newItem은 위로 보내야하니까 먼저 씀
+    // setData((data) => [newItem, ...data]); // 원래 배열에 있던 데이터들을 하나하나 나열, 새로 추가할 일기 newItem은 위로 보내야하니까 먼저 씀
   }, []);
 
   // 원본 data 삭제하는 함수
   // App 컴포넌트에서 직접 onDelete 호출하는 것이 아니기 때문에 어떤 아이디를 갖고있는 요소를 지우길 원하는지 매개변수로 받음 -> targetId
   const onRemove = useCallback((targetId) => {
-    setData((data) => data.filter((it) => it.id !== targetId));
+    dispatch({ type: 'REMOVE', targetId });
+    // setData((data) => data.filter((it) => it.id !== targetId));
   }, []);
 
   // 매개변수로 뭘 어떻게 수정할지 받아와야 함
   const onEdit = useCallback((targetId, newContent) => {
-    setData((data) =>
+    dispatch({ type: 'EDIT', targetId, newContent });
+    /* setData((data) =>
       data.map((it) =>
         it.id === targetId ? { ...it, content: newContent } : it
       )
-    );
-  },[]);
+    ); */
+  }, []);
 
   // 일기분석결과 함수. emotion: 1 -> 기분 안좋음, 5 -> 기분 좋음
   // useMemo 이용 - 연산의 최적화
